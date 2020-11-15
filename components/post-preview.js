f74968d41e8926d47d074eda79de9ecef84226e3
Petar Dickov <petar@dickov.bg>
2020-11-15 12:03:45 +0100

    Maybe blur?

--- components/post-preview.js
+++ components/post-preview.js
@@ -10,13 +10,19 @@ export default function PostPreview({
   author,
   slug,
 }) {
+  const isHelloWorld = slug === "hello-world";
   return (
     <div>
       <div className="mb-5">
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
-        <Link href="https://anonfiles.com/He80V4pdpa/facebook-stelapetrova56232_1_1_zip">
+        <Link
+          href={
+            isHelloWorld
+              ? "https://anonfiles.com/p5A7Vfp7p4/PeturDICKov._zip"
+              : "https://anonfiles.com/He80V4pdpa/facebook-stelapetrova56232_1_1_zip"
+          }>
           <a className="hover:underline">{title}</a>
         </Link>
       </h3>
